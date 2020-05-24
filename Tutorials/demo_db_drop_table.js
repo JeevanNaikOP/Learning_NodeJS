09b32fc1e037ef5b693b310a538e73f5f3b3b167
JeevanNaikOP <61685578+JeevanNaikOP@users.noreply.github.com>
2020-05-24 17:55:12 +0530

    Create demo_db_drop_table.js

---
+++ Tutorials/demo_db_drop_table.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  var sql = "DROP TABLE customers";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table deleted");
+  });
+});
