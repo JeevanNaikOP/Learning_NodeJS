353b14db94b318618e3ad50b4dad5e7b2dadee89
JeevanNaikOP <61685578+JeevanNaikOP@users.noreply.github.com>
2020-05-25 15:55:16 +0530

    Create demo_db_offset_shorter_syntax.js

---
+++ Tutorials/Limit/demo_db_offset_shorter_syntax.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  var sql = "SELECT * FROM customers LIMIT 2, 5";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
