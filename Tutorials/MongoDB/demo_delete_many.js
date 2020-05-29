ae9e373d3d4355008183667296eed1c8b19a50cb
JeevanNaikOP <61685578+JeevanNaikOP@users.noreply.github.com>
2020-05-29 15:58:33 +0530

    Create demo_delete_many.js

---
+++ Tutorials/MongoDB/demo_delete_many.js
@@ -0,0 +1,13 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  var myquery = { address: /^O/ };
+  dbo.collection("customers").deleteMany(myquery, function(err, obj) {
+    if (err) throw err;
+    console.log(obj.result.n + " document(s) deleted");
+    db.close();
+  });
+});
