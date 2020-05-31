d52d9f7b045dbcf13b681be999d771a27510823a
JeevanNaikOP <61685578+JeevanNaikOP@users.noreply.github.com>
2020-05-31 15:06:32 +0530

    Update demo_module.js

--- Tutorials/demo_module.js
+++ Tutorials/demo_module.js
@@ -5,4 +5,4 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("The date and time are currently: " + dt.myDateTime());
   res.end();
-}).listen(8080)
+}).listen(8080);
