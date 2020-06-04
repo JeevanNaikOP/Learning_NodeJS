f26b76eb31fdbdd291e931f4964114f8f2db4a1d
JeevanNaikOP <61685578+JeevanNaikOP@users.noreply.github.com>
2020-06-04 17:03:45 +0530

    Update send_HTML.js

--- Tutorials/Email_send/send_HTML.js
+++ Tutorials/Email_send/send_HTML.js
@@ -12,7 +12,7 @@ var mailOptions = {
   from: 'youremail@gmail.com',
   to: 'myfriend@yahoo.com',
   subject: 'Sending Email using Node.js',
-  html: '<h1>Welcome</h1><p>That was easy!</p>'
+  html: '<h1>Welcome</h1><p>That was easy!</p>
 };
 
 transporter.sendMail(mailOptions, function(error, info){
